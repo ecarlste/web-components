9989e6e0e208d15ca67aa487cafd784a53ec98a4
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-04 11:23:12 -0800

    Added some styling examples for vanilla js components

--- tooltip.js
+++ tooltip.js
@@ -8,13 +8,44 @@ class Tooltip extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <style>
                 div {
+                    font-weight: normal;
                     background-color: black;
                     color: white;
                     position: absolute;
+                    top: 1.5rem;
+                    left: 0.75rem;
                     z-index: 10;
+                    padding: 0.15rem;
+                    border-radius: 3px;
+                    box-shadow: 1px 1px 6px rgba(0, 0, 0, .26);
                 }
+
+                :host(.important) {
+                    background: var(--color-primary, #ccc);
+                    padding: 0.15rem;
+                }
+                
+                :host-context(p) {
+                    font-weight: bold;
+                }
+
+                .hightlight {
+                    background-color: red;
+                }
+
+                ::slotted(.highlight) {
+                    border-bottom: 1px dotted red;
+                  }
+      
+                  .icon {
+                    background: black;
+                    color: white;
+                    padding: 0.15rem 0.5rem;
+                    text-align: center;
+                    border-radius: 50%;
+                  }
             </style>
-            <slot></slot>
+            <slot>Default slot value</slot>
             <span> (?) </span>
         `;
     }
