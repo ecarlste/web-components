0355dd9f61239c3b124e7af6bdb2132457620b1c
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-03 20:06:18 -0800

    Extracted info box toggle into a standalone component

---
+++ info-box-toggle.js
@@ -0,0 +1,48 @@
+class InfoBoxToggle extends HTMLElement {
+    constructor() {
+        super();
+        
+        this.attachShadow({ mode: 'open' });
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                p {
+                    display: none;
+                }
+            </style>
+            <button>Show</button>
+            <p id="info-box">More infos!</p>
+        `;
+    }
+
+    connectedCallback() {
+        this.button = this.shadowRoot.querySelector('button');
+        this.infoEl = this.shadowRoot.querySelector('p');
+
+        this.button.addEventListener('click', this._toggleInfoBoxVisibility.bind(this));
+
+        this.isHidden = true;
+    }
+
+    _toggleInfoBoxVisibility() {
+        if (this.isHidden) {
+            this._showInfoBox();
+        } else {
+            this._hideInfoBox();
+        }
+    }
+
+    _showInfoBox() {
+        this.infoEl.style.display = 'block';
+        this.button.textContent = 'Hide';
+        this.isHidden = false;
+    }
+
+    _hideInfoBox() {
+        this.infoEl.style.display = 'none';
+        this.button.textContent = 'Show';
+        this.isHidden = true;
+    }
+}
+
+customElements.define('codecartel-info-box-toggle', InfoBoxToggle);
