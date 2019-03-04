b07331635eec79c592f9c758bc9b7fffd06ee78b
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-03 16:13:42 -0800

    Added text attribute to tooltip component with default value

--- tooltip.js
+++ tooltip.js
@@ -2,19 +2,25 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipContainer;
+        this._tooltipText = 'Default tooltip text...';
     }
 
     connectedCallback() {
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+
+        this.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        this.addEventListener('mouseenter', this._showTooltip.bind(this));
+        
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?) ';
-        this.addEventListener('mouseenter', this._showTooltip.bind(this));
-        this.addEventListener('mouseleave', this._hideTooltip.bind(this));
         this.appendChild(tooltipIcon);
     }
 
     _showTooltip = () => {
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is the tooltip text!';
+        this._tooltipContainer.textContent = this._tooltipText;
         this.appendChild(this._tooltipContainer);
     }
 
