c6ca01fccf1290d83bd8ed8f5372e852716a980a
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-05 15:04:49 -0800

    Implemented rudimentary form validation

--- web-components-stencil/src/components/stock-price/stock-price.tsx
+++ web-components-stencil/src/components/stock-price/stock-price.tsx
@@ -11,12 +11,19 @@ export class StockPrice {
   stockInput: HTMLInputElement;
 
   @State() price: number;
+  @State() stockUserInput
+  @State() isStockInputValid = false;
 
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol" ref={element => this.stockInput = element} />
-        <button type="submit">Fetch</button>
+        <input
+          id="stock-symbol"
+          ref={element => this.stockInput = element}
+          value={this.stockUserInput}
+          onInput={this.onUserInput.bind(this)}
+        />
+        <button type="submit" disabled={!this.isStockInputValid}>Fetch</button>
       </form>,
       <div>
         <p>Price: ${this.price}</p>
@@ -39,5 +46,11 @@ export class StockPrice {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  onUserInput(event: Event) {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+
+    this.isStockInputValid = this.stockUserInput.trim() !== '' ? true : false;
   }
 }
