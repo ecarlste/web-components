55780e4c5ecd45abc1e3febd53029641908a4a4d
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-05 08:35:48 -0800

    Converted project to use 2 spaces for tabs

--- web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -1,73 +1,73 @@
 import { Component, Prop, State, Method } from "@stencil/core";
 
 @Component({
-    tag: 'codecartel-side-drawer',
-    styleUrl: './side-drawer.css',
-    shadow: true
+  tag: 'codecartel-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true
 })
 export class SideDrawer {
-    @State() showContactInfo = false;
+  @State() showContactInfo = false;
 
-    @Prop({ reflectToAttr: true }) headerTitle: string;
-    @Prop({ reflectToAttr: true, mutable: true }) isOpen: boolean;
-    
-    render() {
-        let mainContent = <slot />;
+  @Prop({ reflectToAttr: true }) headerTitle: string;
+  @Prop({ reflectToAttr: true, mutable: true }) isOpen: boolean;
 
-        if (this.showContactInfo) {
-            mainContent = (
-                <div id="contact-info">
-                    <h2>Contact Information</h2>
-                    <p>You can reach us via phone or email.</p>
-                    <ul>
-                        <li>Phone: 4068675309</li>
-                        <li>
-                            E-Mail:{' '}
-                            <a href="mailto:something@something.com">something@something.com</a>
-                        </li>
-                    </ul>
-                </div>
-            );
-        }
+  render() {
+    let mainContent = <slot />;
 
-        return ([
-            <div class="backdrop" onClick={this.onCloseDrawer.bind(this)} />,
-            <aside>
-                <header>
-                    <h1>{this.headerTitle}</h1>
-                    <button onClick={this.onCloseDrawer.bind(this)}>X</button>
-                </header>
-                <section id="tabs">
-                    <button
-                        class={this.showContactInfo ? '' : 'active'}
-                        onClick={this.onContentChange.bind(this, 'nav')}
-                    >
-                        Navigation
+    if (this.showContactInfo) {
+      mainContent = (
+        <div id="contact-info">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+          <ul>
+            <li>Phone: 4068675309</li>
+            <li>
+              E-Mail:{' '}
+              <a href="mailto:something@something.com">something@something.com</a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+    return ([
+      <div class="backdrop" onClick={this.onCloseDrawer.bind(this)} />,
+      <aside>
+        <header>
+          <h1>{this.headerTitle}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <section id="tabs">
+          <button
+            class={this.showContactInfo ? '' : 'active'}
+            onClick={this.onContentChange.bind(this, 'nav')}
+          >
+            Navigation
                     </button>
-                    <button
-                        class={this.showContactInfo ? 'active' : ''}
-                        onClick={this.onContentChange.bind(this, 'contact')}
-                    >
-                        Contact
+          <button
+            class={this.showContactInfo ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'contact')}
+          >
+            Contact
                     </button>
-                </section>
-                <main>
-                    {mainContent}
-                </main>
-            </aside>
-        ]);
-    }
+        </section>
+        <main>
+          {mainContent}
+        </main>
+      </aside>
+    ]);
+  }
 
-    onCloseDrawer() {
-        this.isOpen = false;
-    }
+  onCloseDrawer() {
+    this.isOpen = false;
+  }
 
-    onContentChange(content: string) {
-        this.showContactInfo = content === 'contact';
-    }
+  onContentChange(content: string) {
+    this.showContactInfo = content === 'contact';
+  }
 
-    @Method()
-    open() {
-        this.isOpen = true;
-    }
+  @Method()
+  open() {
+    this.isOpen = true;
+  }
 }
